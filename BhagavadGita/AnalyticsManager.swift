ceef2435f98e014fbff38595160056e1e84c45de
Waheed Afolabi <waheed.freelancer16@gmail.com>
2023-11-04 21:47:30 +0100

    untrack the Firebase plist files

---
+++ BhagavadGita/AnalyticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsManager.swift
+//  BhagavadGita
+//
+//  Created by waheedCodes on 04/11/2023.
+//
+
+import Foundation
---
+++ BhagavadGita/FirebaseAnalyticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseAnalyticsManager.swift
+//  BhagavadGita
+//
+//  Created by waheedCodes on 03/11/2023.
+//
+
+import Foundation
